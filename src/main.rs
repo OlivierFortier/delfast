67069e97373cab5e7ddcfc4cc3a6ef4b502e10b5
Olivier Fortier <olivier_fortier@outlook.com>
2023-01-21 19:55:43 -0500

    add color formating

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@ use std::fs;
 use std::env;
 
 fn main() {
+    // Get the command line arguments and the current working directory
     let args: Vec<_> = env::args().collect();
     let current_path = env::current_dir().unwrap();
 
-    println!("============================");
-    println!("CWD is : {}", &current_path.display());
-    println!("============================");
+    let green = "\x1b[32m";
+    let reset = "\x1b[0m";
+    let bold = "\x1b[1m";
 
+    println!("{} {} ====================================================", green, bold);
+    println!("CWD is : {}", &current_path.display());
+    println!("------------------------------------------------------");
+
+    // Get the path of the folder to be deleted
     let path = current_path.join(&args[1]);
     println!("Folder to be deleted : {}", &path.display());
+    println!("==================================================== {}" , reset);
+
+    // Delete the folder
     fs::remove_dir_all(path).unwrap();
 }

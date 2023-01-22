26c93095dc61383b2715d0fd8e0d34cf75347d79
Olivier Fortier <olivier_fortier@outlook.com>
2023-01-21 20:06:06 -0500

    new version with better formatting

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,21 @@ fn main() {
     let current_path = env::current_dir().unwrap();
 
     let green = "\x1b[32m";
+    let blue = "\x1b[34m";
     let reset = "\x1b[0m";
     let bold = "\x1b[1m";
 
-    println!("{} {} ====================================================", green, bold);
-    println!("CWD is : {}", &current_path.display());
-    println!("------------------------------------------------------");
+    println!("{}{}====================================================", green, bold);
+    println!("CWD is : {}{}", blue, &current_path.display());
+    println!("{}------------------------------------------------------", green);
 
     // Get the path of the folder to be deleted
     let path = current_path.join(&args[1]);
-    println!("Folder to be deleted : {}", &path.display());
-    println!("==================================================== {}" , reset);
+    println!("Folder to be deleted : {}{}", blue, &path.display());
+    println!("{}------------------------------------------------------", green);
 
     // Delete the folder
     fs::remove_dir_all(path).unwrap();
+    println!("Folder deleted successfully!");
+    println!("===================================================={}", reset);
 }

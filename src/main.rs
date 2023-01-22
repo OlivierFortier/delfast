f4818a69b8a6b1979d05f6064e720078c1d192c3
Olivier Fortier <olivier_fortier@outlook.com>
2023-01-21 22:19:24 -0500

    add better error handling using match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::env;
+use std::fs;
 
 fn main() {
     // Get the command line arguments and the current working directory
@@ -12,17 +12,32 @@ fn main() {
     let reset = "\x1b[0m";
     let bold = "\x1b[1m";
 
-    println!("{}{}====================================================", green, bold);
+    println!(
+        "{}{}====================================================",
+        green, bold
+    );
     println!("CWD is : {}{}", blue, &current_path.display());
-    println!("{}------------------------------------------------------", green);
+    println!(
+        "{}------------------------------------------------------",
+        green
+    );
 
     // Get the path of the folder to be deleted
     let path = current_path.join(&args[1]);
     println!("Folder to be deleted : {}{}", blue, &path.display());
-    println!("{}------------------------------------------------------", green);
+    println!(
+        "{}------------------------------------------------------",
+        green
+    );
 
     // Delete the folder
-    fs::remove_dir_all(path).unwrap_or(println!("{}{}Error: Folder '{}' not found!", red, bold, &args[1]));
-    println!("{}Operation performed successfully!", green);
-    println!("===================================================={}", reset);
+    // use pattern matching to handle the error
+    match fs::remove_dir_all(path) {
+        Ok(_) => println!("{}Folder deleted successfully!", green),
+        Err(e) => println!("{}Error [{}]: Folder '{}' not found!", red, e, &args[1]),
+    }
+    println!(
+        "{}===================================================={}",
+        green, reset
+    );
 }

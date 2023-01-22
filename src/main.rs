d44f867229c754fe7aefe3247c33454c41687478
Olivier Fortier <olivier_fortier@outlook.com>
2023-01-21 19:42:09 -0500

    version 0.1.1 of the program

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use std::fs;
+use std::env;
 
 fn main() {
-    let path = r"C:\Users\Olivier\Documents\work\ConnectIt\SPB\spb-portail-maintenance";
+    let args: Vec<_> = env::args().collect();
+    let current_path = env::current_dir().unwrap();
 
+    println!("============================");
+    println!("CWD is : {}", &current_path.display());
+    println!("============================");
+
+    let path = current_path.join(&args[1]);
+    println!("Folder to be deleted : {}", &path.display());
     fs::remove_dir_all(path).unwrap();
 }

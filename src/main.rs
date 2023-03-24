28a2b842b42d7a6c0d66134e8f2518366cf8a4c7
Olivier Fortier <olivier_fortier@outlook.com>
2023-03-23 23:38:36 -0400

    try adding context menu cross-platform

--- src/main.rs
+++ src/main.rs
@@ -2,56 +2,80 @@ use clap::Parser;
 use std::env;
 use std::fs;
 
-// #[cfg(target_os = "windows")]
-// mod windows {
-//     use winreg::RegKey;
-//     use winreg::enums::{HKEY_CLASSES_ROOT, KEY_WRITE};
-//     use std::ffi::OsStr;
-//     use std::os::windows::ffi::OsStrExt;
+#[cfg(target_os = "windows")]
+mod windows {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+    use winreg::enums::{HKEY_CLASSES_ROOT, KEY_WRITE};
+    use winreg::RegKey;
 
-//     pub fn add_context_menu() {
-//         let path = "Directory\\Background\\shell\\delfast";
-//         let key = RegKey::predef(HKEY_CLASSES_ROOT)
-//             .create_subkey(path)
-//             .unwrap();
-//         key.set_value("", &"delfast").unwrap();
-//         let command_key = key.create_subkey("command").unwrap();
-//         command_key.set_value("", &format!("delfast.exe %1")).unwrap();
-//     }
-// }
+    pub fn add_context_menu() {
+        let path = "Directory\\Background\\shell\\delfast";
+        let key = RegKey::predef(HKEY_CLASSES_ROOT)
+            .create_subkey(path)
+            .unwrap();
+        key.set_value("", &"delfast").unwrap();
+        let command_key = key.create_subkey("command").unwrap();
+        command_key
+            .set_value("", &format!("delfast.exe %1"))
+            .unwrap();
+    }
 
-// #[cfg(target_os = "macos")]
-// mod macos {
-//     use cocoa::base::{id, nil};
-//     use cocoa::appkit::{NSStatusItem, NSStatusBar};
-//     use cocoa::foundation::{NSString};
+    pub fn is_context_menu_installed() -> bool {
+        let path = "Directory\\Background\\shell\\delfast";
+        let key = RegKey::predef(HKEY_CLASSES_ROOT).open_subkey(path).is_ok();
+        key
+    }
+}
 
-//     pub fn add_context_menu() {
-//         let bar = NSStatusBar::systemStatusBar();
-//         let item = bar.statusItemWithLength(30.0);
-//         item.setTitle(NSString::alloc(nil).init_str("delfast"));
-//         item.setAction("open:".to_owned());
-//     }
-// }
+#[cfg(target_os = "macos")]
+mod macos {
+    use cocoa::appkit::{NSStatusBar, NSStatusItem};
+    use cocoa::base::{id, nil};
+    use cocoa::foundation::{NSAutoreleasePool, NSString};
 
-// #[cfg(target_os = "linux")]
-// mod linux {
-//     use xdg::BaseDirectories;
-//     use std::fs;
-//     use std::path::Path;
+    pub fn add_context_menu() {
+        let pool = unsafe { NSAutoreleasePool::new(nil) };
+        let bar = NSStatusBar::systemStatusBar();
+        let item = bar.statusItemWithLength(30.0);
+        item.setTitle(NSString::alloc(nil).init_str("delfast"));
+        item.setAction("open:".to_owned());
+        drop(pool);
+    }
 
-//     pub fn add_context_menu() {
-//         let xdg_dirs = BaseDirectories::new().unwrap();
-//         let path = xdg_dirs.place_data_file("delfast/delfast.desktop").unwrap();
-//         let menu_entry = "[Desktop Entry]
-//         Name=delfast
-//         Exec=delfast %f
-//         Type=Application
-//         NoDisplay=true
-//         Categories=Utility;";
-//         fs::write(path, menu_entry).unwrap();
-//     }
-// }
+    pub fn is_context_menu_installed() -> bool {
+        // TODO: Implement check for context menu installation on MacOS
+        false
+    }
+}
+
+#[cfg(target_os = "linux")]
+mod linux {
+    use std::fs;
+    use std::path::Path;
+    use xdg::BaseDirectories;
+
+    pub fn add_context_menu() {
+        let xdg_dirs = BaseDirectories::new().unwrap();
+        let path = xdg_dirs.place_data_file("delfast/delfast.desktop").unwrap();
+        let menu_entry = "[Desktop Entry]
+Name=delfast
+Exec=delfast %f
+Type=Application
+NoDisplay=true
+Categories=Utility;";
+        fs::write(path, menu_entry).unwrap();
+    }
+
+    pub fn is_context_menu_installed() -> bool {
+        let xdg_dirs = BaseDirectories::new().unwrap();
+        let path = xdg_dirs
+            .place_data_file("applications/delfast.desktop")
+            .unwrap();
+        Path::new(&path).exists()
+    }
+}
+
 #[derive(Parser, Debug)]
 #[command(name="delfast", author, version, about, long_about = None)]
 struct Args {
@@ -61,6 +85,10 @@ struct Args {
 
     /// Path to the folder to be deleted (relative or absolute)
     path: String,
+
+    /// Install the context menu
+    #[arg(long)]
+    install_context_menu: bool,
 }
 
 // Formatting strings
@@ -71,21 +99,39 @@ const RESET: &str = "\x1b[0m";
 const BOLD: &str = "\x1b[1m";
 
 fn main() {
-
-    // TODO: Add context menu for Windows, MacOS and Linux
-    // #[cfg(target_os = "windows")]
-    // windows::add_context_menu();
-
-    // #[cfg(target_os = "macos")]
-    // macos::add_context_menu();
-
-    // #[cfg(target_os = "linux")]
-    // linux::add_context_menu();
-
     // Get the command line arguments and the current working directory
     let args = Args::parse();
     let current_working_directory = env::current_dir().unwrap();
     let path = current_working_directory.join(&args.path);
+
+    if args.install_context_menu {
+        // Check if context menu is installed
+        #[cfg(target_os = "windows")]
+        let context_menu_installed = windows::is_context_menu_installed();
+
+        #[cfg(target_os = "macos")]
+        let context_menu_installed = macos::is_context_menu_installed();
+
+        #[cfg(target_os = "linux")]
+        let context_menu_installed = linux::is_context_menu_installed();
+
+        // If context menu is not installed, install it
+        if !context_menu_installed {
+            #[cfg(target_os = "windows")]
+            windows::add_context_menu();
+            #[cfg(target_os = "macos")]
+            macos::add_context_menu();
+            #[cfg(target_os = "linux")]
+            linux::add_context_menu();
+
+            // write a message to the console to indicate that the context menu was installed successfully
+            println!("{}Context menu installed successfully!{}", GREEN, RESET);
+            std::process::exit(1);
+        }
+
+        println!("{}Context menu installed successfully!{}", GREEN, RESET);
+        std::process::exit(0);
+    }
 
     print_header(current_working_directory, &path);
 
@@ -115,7 +161,10 @@ fn delete_folder(path: std::path::PathBuf) {
 
 fn confirm_prompt() -> bool {
     print_line(GREEN);
-    println!("{}{}Are you sure you want to delete this folder? [y/n] {}{}", RED, BOLD, RESET, RESET);
+    println!(
+        "{}{}Are you sure you want to delete this folder? [y/n] {}{}",
+        RED, BOLD, RESET, RESET
+    );
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     // if the user enters 'y' or 'Y', return true
